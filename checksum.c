a34fef392534050f17fe22d192873a0f9f8c1bc2
Hua-Yuan <e61983+git@gmail.com>
2016-07-03 17:03:19 +0800

    add an enum for explain running status

--- checksum.c
+++ checksum.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 #include <stdint.h>
 
+enum{
+    SUCCESS,
+    MISSING_FILENAME,
+    FILE_MAYBE_NOT_EXIST
+}e_clac_status;
+
 /**
  * @brief Program Entry
  *
@@ -10,32 +16,34 @@
  * @return
  *  return program status
  * @retval
- *  0 - Success
- *  1 - Fail
- * @return
+ * refer: enum e_clac_status
  */
+
 int main(int argc, const char *argv[]) {
 
     /* user will input the filename. we check this file is exist realy.
      * if not, we will resopne some message. */
     if( 1 >= argc ){
         printf("Please input the filename.\n");
-        return 1;
+        return MISSING_FILENAME;
     }
 
     FILE* f_target = NULL;
     if(NULL == (f_target=fopen(argv[1], "rb"))){
         printf("The file \"%s\" maybe not exist.\n",argv[1]);
-        return 2;
+        return FILE_MAYBE_NOT_EXIST;
     }
 
-    uint32_t checksum = 0;
+    /* calculating checksum */
 
     int i_readforsum = EOF;
+    uint32_t checksum = 0;
     while (EOF !=(i_readforsum = fgetc(f_target))){
         checksum += i_readforsum;
     }
     fclose(f_target);
+
     printf("0x%x\n",checksum);
 
+    return SUCCESS;
 }
